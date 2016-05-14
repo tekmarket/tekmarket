03b7522cf722b98d545bad57344195e510fa6862
ertekinsoft <ertekinsoft@gmail.com>
2016-05-14 18:59:46 +0300

    admin Girişi

--- teknolojiMarket/teknolojiMarket/Controllers/AdminController.cs
+++ teknolojiMarket/teknolojiMarket/Controllers/AdminController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using teknolojiMarket.Models;
 
 namespace teknolojiMarket.Controllers
 {
@@ -11,6 +13,24 @@ namespace teknolojiMarket.Controllers
         // GET: Adminds
         public ActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string adminNik,string adminPass)
+        {
+            string sqlSorugum = "SELECT * FROM Yonetici WHERE nik='" + adminNik + "' AND sifre='" + adminPass + "'";
+            CodeDB cntrl = new CodeDB();
+            DataTable sqlSonuc = cntrl.SqlSorgu(sqlSorugum);
+
+            if (sqlSonuc.Rows.Count != 0)
+            {
+                Yonetici y = new Yonetici(sqlSonuc);
+                Session["yonetici"] = y;
+                return RedirectToAction("Panel", "Admin");
+            }
+            else {
+                Session["yonetici"] = null;
+            }         
             return View();
         }
         public ActionResult Panel()
---
+++ teknolojiMarket/teknolojiMarket/Models/Yonetici.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+namespace teknolojiMarket.Models
+{
+    public class Yonetici
+    {
+        public string adi { get; set; }
+        public string soyadi { get; set; }
+        public string nik { get; set; }
+        public string sifre { get; set; }
+        public string eposta { get; set; }
+        public string tcno { get; set; }
+        public int kullaniciID { get; set; }
+        public Yonetici(DataTable dt)
+        {
+            if (dt.IsInitialized && dt != null)
+            {
+                adi = dt.Rows[0]["adi"].ToString();
+                soyadi = dt.Rows[0]["soyadi"].ToString();
+                nik = dt.Rows[0]["nik"].ToString();
+                sifre = dt.Rows[0]["sifre"].ToString();
+                eposta = dt.Rows[0]["eposta"].ToString();
+                tcno = dt.Rows[0]["tcno"].ToString();
+                kullaniciID = Convert.ToInt32(dt.Rows[0]["kullaniciID"].ToString());
+            }
+
+        }
+
+    }
+}
