ce1ed6da7a2827906c05a15efe1463b44168daa3
tanersimsek <tnrsmsk@yandex.com>
2016-05-15 10:41:06 +0300

    adresekle eklendi

--- teknolojiMarket/teknolojiMarket/Controllers/HomeController.cs
+++ teknolojiMarket/teknolojiMarket/Controllers/HomeController.cs
@@ -23,6 +23,29 @@ namespace teknolojiMarket.Controllers
             }
             return View();
         }
-
+        public ActionResult AdresEkle()
+        {
+            if (Session["musteri"] == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            Session["htmladresekle"] = " ";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AdresEkle(string us2lon,string us2lat,string us2address)
+        {
+            Musteri a = Session["musteri"] as Musteri;
+           
+            
+            CodeDB cntrl2 = new CodeDB();
+            string sqlsorgum6 = "INSERT INTO Adres(baslik,enlem,boylam,kid) values ('" + us2address + "','" + us2lat + "','" + us2lon + "'," + a.kullaniciID + ")";
+            if (cntrl2.SqlKomut(sqlsorgum6))
+            {
+                Session["htmladresekle"] = "Başarılı bir şekilde eklendi";
+            }
+            else { Session["htmladresekle"] = "Eklenemedi "; }
+            return View();
+        }
     }
 }
